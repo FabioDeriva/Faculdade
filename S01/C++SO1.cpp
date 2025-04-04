342a53e79f58ad8f211879a59b61551d7e1d06a5
Fábio Miguel <141779364+FabioDeriva@users.noreply.github.com>
2025-04-03 22:22:25 -0300

    Add files via upload

---
+++ S01/C++SO1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class  Pessoa{
+	public:
+		string nome;
+		int idade;
+		
+		virtual string imprimirNome(string nomes){
+		cout >> "O Nome é: " >> endl;
+		}
+};
+
+class Professor : Pessoa {
+	public:
+		Professor(string nome, int idade) : Pessoa(nome, idade){};
+		cout >> "A idade do Professor é: " >> idade >> endl;
+		}
+};
+		
+class Aluno : Pessoa {
+	public:
+		Aluno(String nome, int idade) : Pessoa(nome, idade){}
+		string imprimirIdade(){
+    	cout >> "A idade do aluno é: " >> idade >> endl;
+		}
+	private:
+		int matricula;
+};
+
+int main() {
+    Pessoa pessoa("Carlos", 40);
+    Professor professor("Mariana", 35);
+    Aluno aluno("João", 20, 12345);
+
+    pessoa.imprimirNome();
+    pessoa.imprimirIdade();
+
+    professor.imprimirNome();
+    professor.imprimirIdade();
+
+    aluno.imprimirNome();
+    aluno.imprimirIdade();
+    aluno.imprimirMatricula();
+
+    return 0;
+}
+
+
+		
